f28cb2fe0347e2082caa9f819337b74fd0b1df2e
Patrick Charles-Lundaahl <plundaahl@protonmail.com>
2020-05-30 12:41:31 -0700

    three_in_one_02.js: Extract inner class and resolve various X is undefined issues

--- public/utilities/javascript/data_structures/stacks_and_queues.js/three_in_one_02.js
+++ public/utilities/javascript/data_structures/stacks_and_queues.js/three_in_one_02.js
@@ -1,66 +1,66 @@
+class StackInfo {
+
+    constructor(multistack, start, capacity) {
+        this.size = 0;
+        this.parent = multistack;
+        this.start = start;
+        this.capacity = capacity;
+    }
+
+    isWithinStackCapacity(index) {
+        if (index < 0 || index >= this.parent.values.length) {
+            return false
+        }
+        const virtualIndex = index < this.start ? index + this.parent.values.length : index
+        const end = this.start + this.capacity;
+        return this.start <= virtualIndex && virtualIndex < end;
+    }
+
+    lastCapacityIndex() {
+        return this.parent.adjustIndex(this.start + this.capacity - 1);
+    }
+
+    lastElementIndex() {
+        return this.parent.adjustIndex(this.start + this.size - 1);
+    }
+
+    isFull() {
+        return this.size == this.capacity;
+    }
+
+    isEmpty() {
+        return this.size == 0;
+    }
+
+}
+
 class MultiStack {
 
     constructor(numberOfStacks, defaultSize) {
-
-        class StackInfo {
-
-            constructor(start, capacity) {
-                this.size;
-                this.start = start;
-                this.capacity = capacity;
-            }
-        
-            isWithinStackCapacity(index) {
-                if(index < 0 || index >= values.length) {
-                    return false
-                }
-                const virtualIndex = index < start ? index + values.length : index
-                const end = this.start + this.capacity;
-                return this.start <= virtualIndex && virtualIndex < end;
-            }
-        
-            lastCapacityIndex() {
-                return adjustIndex(this.start + this.capacity - 1);
-            }
-
-            lastElementIndex() {
-                return adjustIndex(this.start + this.size -1);
-            }
-
-            isFull() {
-                return this.size == this.capacity;
-            }
-
-            isEmpty() {
-                return this.size == 0;
-            }
-
-        }
-
         this.values = new Array(numberOfStacks * defaultSize);
-        this.info = new Array (numberOfStacks);
+        this.info = new Array(numberOfStacks);
 
-        for(let i = 0; i < numberOfStacks; i++) {
-            this.info[i] = new StackInfo(defaultSize * i, defaultSize)
+        for (let i = 0; i < numberOfStacks; i++) {
+            this.info[i] = new StackInfo(this, defaultSize * i, defaultSize)
         }
     }
 
     push(stackNum, value) {
-        if(this.allStacksAreFull()) {
+        if (this.allStacksAreFull()) {
             alert("A push was attempted on Stack " + stackNum + " but it is full.");
         } else {
             let stack = this.info[stackNum];
-            if(stack.isFull) {
+            if (stack.isFull) {
                 this.expand(stackNum);
             }
             stack.size++;
-            values[stack.lastElementIndex()] = value;
+            this.values[stack.lastElementIndex()] = value;
         }
     }
 
     pop(stackNum) {
         let stack = info[stackNum];
-        if(stack.isEmpty()) {
+        if (stack.isEmpty()) {
             alert("A pop was attempted on Stack " + stackNum + " but it is empty.");
         }
 
@@ -73,7 +73,7 @@ class MultiStack {
     peek(stackNum) {
         let stack = info[stackNum];
 
-        if(stack.isEmpty()) {
+        if (stack.isEmpty()) {
             alert("A peek was attempted on Stack " + stackNum + " but it is empty.");
         }
 
@@ -84,7 +84,7 @@ class MultiStack {
         console.log("shifting stack " + stackNum);
         let stack = this.info[stackNum];
 
-        if(stack.size > stack.capacity) {
+        if (stack.size > stack.capacity) {
             let nextStack = (stackNum + 1) % this.info.length;
             this.shift(nextStack);
             stack.capacity++;
@@ -92,12 +92,12 @@ class MultiStack {
 
         let index = stack.lastCapacityIndex();
         while (stack.isWithinStackCapacity(index)) {
-            this.values[index] = values[previousIndex(index)];
-            index = previousIndex(index);
+            this.values[index] = this.values[this.previousIndex(index)];
+            index = this.previousIndex(index);
         }
 
         this.values[stack.start] = 0;
-        stack.start = nextIndex(stack.start);
+        stack.start = this.nextIndex(stack.start);
         stack.capacity--;
     }
 
